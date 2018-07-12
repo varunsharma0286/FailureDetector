5aad5b02747ffc90855da4e83aa1d5e2a465c123
varunsharma0286 <vsharma7@buffalo.edu>
2018-07-11 22:17:05 -0400

    Initial file

---
+++ EntityMain.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+	if(argc != 2)
+	{
+		cout<<"Incorrect usage"<<endl;
+		cout<<"Correct usage:./"<<argv[0]<<" <Config_file_Path>"<<endl;
+		return 0;
+	}
+
+}
